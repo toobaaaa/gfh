13492c9ad73769cb6943bba2ff30350b7c229371
toobaaaa <87388669+toobaaaa@users.noreply.github.com>
2021-07-15 14:58:52 -0700

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,66 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile2 = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . 1 1 1 1 1 4 4 4 . . . . 
+        . . . . 1 1 1 1 1 4 4 4 . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 100, 0)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Projectile, function (sprite, otherSprite) {
+    otherSprite.destroy()
+})
+let projectile2: Sprite = null
+let mySprite: Sprite = null
 scene.setBackgroundColor(1)
 tiles.setTilemap(tilemap`level1`)
-let mySprite = sprites.create(img`
-    . . . . . . . e . e . . . . . . 
-    . . . . . . e 3 e 3 e . . . . . 
-    . . . . . . e e e e e . . . . . 
-    . . . . . . e f e f e . . . . . 
-    . . . . . . f e f e f . . . . . 
-    . . . . . . e f f f e . . . . . 
-    . . . . . e e e e e e e . . . . 
-    . . f e e e e e e e e e e e f . 
-    . . f e e e e e e e e e e e f . 
-    . . . . . e e e e e e e . . . . 
-    . . . . . e e e e e e e . . . . 
-    . . . . . e e e e e e e . . . . 
-    . . . . . e e e e e e e . . . . 
-    . . . . . . e e . e e . . . . . 
-    . . . . . . f f . f f . . . . . 
+mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
+    . . . . e e . e e . . . . . . . 
+    . . . . e e e e e . . . . . . . 
+    . . . . e e e e f . . . . . . . 
+    . . . . e e e e e f . . . . . . 
+    . . . . e e e e e e . . . . . . 
+    . . . . e e e e e . . . . . . . 
+    . . . e e e e e e e . . . . . . 
+    . . . e e f e f e e . . . . . . 
+    . . . e e f e f e e . . . . . . 
+    . . . e e f e f e e . . . . . . 
+    . . . e e f f f e e . . . . . . 
+    . . . e e e e e e e . . . . . . 
+    . . e e e e e e e e . . . . . . 
+    . e e . e e . e e . . . . . . . 
+    . . . . e e . e e . . . . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(mySprite)
+tiles.placeOnRandomTile(mySprite, assets.tile`myTile3`)
+controller.moveSprite(mySprite, 100, 50)
 scene.cameraFollowSprite(mySprite)
+let projectile = sprites.createProjectileFromSide(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 1 1 1 1 1 . . . . 
+    . . . . . . . 1 . . . 1 . . . . 
+    . . . . f f 1 1 f f 5 1 . . . . 
+    . . . 5 f f 1 5 f f 5 1 f . . . 
+    f f 5 5 f f 1 5 f f 5 1 f f . . 
+    f f 5 5 f f 1 5 f f 1 1 f f f . 
+    f f 5 5 f f 5 5 f 1 1 5 f f f f 
+    f f 5 5 f f 5 5 f f 5 5 f f f . 
+    f f 5 5 f f 5 5 f f 5 5 f f . . 
+    . . . 5 f f 5 5 f f 5 5 f . . . 
+    . . . . f f 5 5 f f 5 5 . . . . 
+    . . . . . f . f . f . f . . . . 
+    . . . . f f f f f f f f . . . . 
+    `, randint(-50, 50), randint(-50, 50))
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -6,28 +6,28 @@ namespace myTiles {
     export const tile1 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "level1":
-            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001010100000001010101000000000000000000000000000000000000000000000000010101000000000101000101010000000000000001010000000000000001010100000000000000000001010101`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. 2 2 2 . . . 2 2 2 2 . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . 2 2 2 . . . . 2 2 . 2 2 2 
-. . . . . . . 2 2 . . . . . . . 
-2 2 2 . . . . . . . . . 2 2 2 2 
-`, [myTiles.transparency16,myTiles.tile2], TileScale.Sixteen);
+            case "level1":return tiles.createTilemap(hex`2000080000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000101000000000000000000000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000010000000000000001000000000000000000010101000000000001010000000000000000000000010100000101010000000000000003000000000000000101010000000000000101010000000000000000000000020101010104040404040404040404040401010101040404040404040101010101`, img`
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . 2 2 . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . 2 . . . . . . . . 
+. . . . . . . . . . . 2 . . . . . . . 2 . . . . . . . . . 2 2 2 
+. . . . . 2 2 . . . . . . . . . . . 2 2 . . 2 2 2 . . . . . . . 
+. . . . . . . . 2 2 2 . . . . . . 2 2 2 . . . . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,myTiles.tile2,myTiles.tile4,myTiles.tile5,myTiles.tile6], TileScale.Sixteen);
         }
         return null;
     })
@@ -39,6 +39,14 @@ namespace myTiles {
             case "tile1":return tile1;
             case "myTile0":
             case "tile2":return tile2;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile3":
+            case "tile5":return tile5;
+            case "myTile4":
+            case "tile6":return tile6;
         }
         return null;
     })
